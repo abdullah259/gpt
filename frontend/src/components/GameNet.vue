9cb86ec910c6e37b3a6001e0caeddedff576cdcf
abdullah <abdullahghazi404@gmail.com>
2023-09-10 11:22:08 +0400

    check

---
+++ frontend/src/components/GameNet.vue
@@ -0,0 +1,20 @@
+<template>
+    <!-- The net can be a series of small rectangles, so you might loop through them in the future if needed -->
+    <rect :x="x" :y="y" :width="width" :height="height" :fill="color" />
+</template>
+
+<script lang="ts">
+export default {
+    props: {
+        x: Number,
+        y: Number,
+        width: Number,
+        height: Number,
+        color: String
+    }
+}
+</script>
+
+<style scoped>
+/* Add any specific styles for the net here */
+</style>
---
+++ frontend/src/components/GamePaddle.vue
@@ -0,0 +1,20 @@
+
+<template>
+    <rect :x="x" :y="y" :width="width" :height="height" :fill="color" />
+</template>
+
+<script lang="ts">
+export default {
+    props: {
+        x: Number,
+        y: Number,
+        width: Number,
+        height: Number,
+        color: String
+    }
+}
+</script>
+
+<style scoped>
+/* Add any specific styles for the paddle here */
+</style>
